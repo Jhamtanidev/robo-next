9f02fbc12fde6675122378834a575698128a3832
Jhamtanidev <jhamtanidev2004@gmail.com>
2023-10-15 16:15:07 +0530

    fresh2

--- src/app/dashboard/[para]/page.js
+++ src/app/dashboard/[para]/page.js
@@ -8,7 +8,7 @@ import Alreco from "@/components/parameters/Alreco";
 import Tds from "@/components/parameters/Tds";
 import Sidebar from "@/components/Sidebar/sidebar";
 import RovStats from "@/components/parameters/rovstats";
-import Reletpos from "@/components/parameters/Reletpos";
+import Reletpos from "@/components/parameters/reletpos";  
 
 const page = ({ params }) => {
   let content;

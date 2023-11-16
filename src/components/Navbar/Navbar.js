4c60487f98912ed9a5ef3aad6873f28514024d51
Jhamtanidev <92734164+Jhamtanidev@users.noreply.github.com>
2023-11-16 08:07:07 +0000

    navbar now visible, styling left

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,14 +12,14 @@ function NavBar() {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   const toggleNavbar = () => {
-    setNavbarOpen(!navbarOpen);
+    setNavbarOpen(state => !state);
   };
   const { initial, user, view, signOut } = useAuth();
 
   return (
     <div>
       <nav className=" nav  left-0 right-0 top-0 z-10 h-20 w-full bg-transparent font-montserrat shadow-xl ">
-        <div className="mx-auto justify-between px-4 md:flex md:items-center md:px-8 lg:max-w-7xl">
+        <div className="mx-auto flex flex-col justify-between md:flex-row md:items-center md:px-8 lg:max-w-7xl">
           <div>
             <div className="flex items-center justify-between md:block">
               {/* LOGO */}
@@ -57,62 +57,59 @@ function NavBar() {
               </div>
             </div>
           </div>
-          <div>
-            <div
-              className={`mt-8 flex-1 justify-self-center pb-3 md:mt-0 md:block md:pb-0 ${
-                navbarOpen ? "block p-12 md:p-0" : "hidden"
+          <div
+            className={`mt-3 flex-1 z-50 p-12 pb-3 bg-white md:bg-transparent md:mt-0 md:block md:p-0 md:pb-0 ${navbarOpen ? "block" : "hidden"
               }`}
-            >
-              <ul className="h-screen items-center justify-center md:flex md:h-auto ">
-                <li className="border-b-2 border-purple-900 py-2 text-center text-xl text-color-primary hover:bg-purple-900  md:border-b-0  md:px-6  md:hover:bg-transparent md:hover:text-purple-600">
-                  <Link href="/about" onClick={() => setNavbar(!navbar)}>
-                    Team
-                  </Link>
-                </li>
+          >
+            <ul className="h-screen items-center justify-center md:flex md:h-auto ">
+              <li className="border-b-2 border-purple-900 py-2 text-center text-xl text-color-primary hover:bg-purple-900  md:border-b-0  md:px-6  md:hover:bg-transparent md:hover:text-purple-600">
+                <Link href="/about" onClick={() => setNavbar(!navbar)}>
+                  Team
+                </Link>
+              </li>
 
-                <li className="border-b-2 border-purple-900 px-6 py-2 text-center  text-xl text-color-primary  hover:bg-purple-600  md:border-b-0  md:hover:bg-transparent md:hover:text-purple-600">
-                  <Link href="/contactus" onClick={() => setNavbar(!navbar)}>
-                    Contact Us
-                  </Link>
-                </li>
-                {/* <li className="border-b-2 border-purple-900 px-6 py-2 text-center  text-xl text-color-primary  hover:bg-purple-600  md:border-b-0  md:hover:bg-transparent md:hover:text-purple-600">
+              <li className="border-b-2 border-purple-900 px-6 py-2 text-center  text-xl text-color-primary  hover:bg-purple-600  md:border-b-0  md:hover:bg-transparent md:hover:text-purple-600">
+                <Link href="/contactus" onClick={() => setNavbar(!navbar)}>
+                  Contact Us
+                </Link>
+              </li>
+              {/* <li className="border-b-2 border-purple-900 px-6 py-2 text-center  text-xl text-color-primary  hover:bg-purple-600  md:border-b-0  md:hover:bg-transparent md:hover:text-purple-600">
                   <Link href="/services" onClick={() => setNavbar(!navbar)}>
                     Services
                   </Link>
                 </li> */}
-                {user ? (
-                  <>
-                    <li className="border-b-2 border-purple-900 px-6 py-2 text-center  text-xl text-color-primary  hover:bg-purple-600  md:border-b-0  md:hover:bg-transparent md:hover:text-purple-600">
-                      <Link href="/profile" onClick={() => setNavbar(!navbar)}>
-                        Profile
-                      </Link>
-                    </li>
-                    <li className="border-b-2 border-purple-900 px-6 py-2 text-center  text-xl text-color-primary  hover:bg-purple-600  md:border-b-0  md:hover:bg-transparent md:hover:text-purple-600">
-                      <Link
-                        href="/dashboard"
-                        onClick={() => setNavbar(!navbar)}
-                      >
-                        Dashboard
-                      </Link>
-                    </li>
-                    <li className="border-b-2 border-purple-900 px-6 py-2 text-center  text-xl text-color-primary  hover:bg-purple-600  md:border-b-0  md:hover:bg-transparent md:hover:text-purple-600">
-                      <Link href="/" onClick={() => setNavbar(!navbar)}>
-                        <SignOut />
-                      </Link>
-                    </li>
-                  </>
-                ) : (
-                  <li className="border-b-2 border-purple-900 px-6 py-2  text-center  text-xl text-color-primary  hover:bg-purple-600  md:border-b-0  md:hover:bg-transparent md:hover:text-purple-600">
-                    <Link
-                      href="/auth/signin"
-                      onClick={() => setNavbar(!navbar)}
-                    >
-                      SignIn
+              {user ? (
+                <>
+                  <li className="border-b-2 border-purple-900 px-6 py-2 text-center  text-xl text-color-primary  hover:bg-purple-600  md:border-b-0  md:hover:bg-transparent md:hover:text-purple-600">
+                    <Link href="/profile" onClick={() => setNavbar(!navbar)}>
+                      Profile
                     </Link>
                   </li>
-                )}
-              </ul>
-            </div>
+                  <li className="border-b-2 border-purple-900 px-6 py-2 text-center  text-xl text-color-primary  hover:bg-purple-600  md:border-b-0  md:hover:bg-transparent md:hover:text-purple-600">
+                    <Link
+                      href="/dashboard"
+                      onClick={() => setNavbar(!navbar)}
+                    >
+                      Dashboard
+                    </Link>
+                  </li>
+                  <li className="border-b-2 border-purple-900 px-6 py-2 text-center  text-xl text-color-primary  hover:bg-purple-600  md:border-b-0  md:hover:bg-transparent md:hover:text-purple-600">
+                    <Link href="/" onClick={() => setNavbar(!navbar)}>
+                      <SignOut />
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                <li className="border-b-2 border-purple-900 px-6 py-2  text-center  text-xl text-color-primary  hover:bg-purple-600  md:border-b-0  md:hover:bg-transparent md:hover:text-purple-600">
+                  <Link
+                    href="/auth/signin"
+                    onClick={() => setNavbar(!navbar)}
+                  >
+                    SignIn
+                  </Link>
+                </li>
+              )}
+            </ul>
           </div>
         </div>
       </nav>
